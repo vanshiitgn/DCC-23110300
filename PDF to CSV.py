bf8214d4881bb4ff64f61ad51e42b806e53b334b
vanshiitgn <167856892+vanshiitgn@users.noreply.github.com>
2024-04-23 14:49:29 +0530

    Update PDF to CSV.py

--- PDF to CSV.py
+++ PDF to CSV.py
@@ -1,38 +1,28 @@
 import fitz
 import pandas as pd
 
-doc = fitz.open("pdf_data/electoralbond2.pdf")
+doc1 = fitz.open("pdf_data/electoralbond1.pdf")
 
-pages = []
-for page in doc:
+pages1 = []
+for page in doc1:
     tables = page.find_tables()
     table = tables[0]
     page_dataframe = table.to_pandas()
-    pages.append(page_dataframe)
+    pages1.append(page_dataframe)
 
-Data = pd.concat(pages)
+Data1 = pd.concat(pages1)
+Data1.to_csv(f"csv_data/electoralbond1.csv",index =False)
+
+
+
+doc2 = fitz.open("pdf_data/electoralbond2.pdf")
+
+pages2 = []
+for page in doc2:
+    tables = page.find_tables()
+    table = tables[0]
+    page_dataframe = table.to_pandas()
+    pages2.append(page_dataframe)
+
+Data = pd.concat(pages2)
 Data.to_csv(f"csv_data/electoralbond2.csv",index =False)
-# print(f"{len(tabs.tables)} table(s) on {page}")
-
-
-
-# import fitz
-# import pandas as pd
-
-# def convert(filename):
-#     doc = fitz.open("pdf_data/"+filename+".pdf")
-#     page_df_lst=[]
-#     for page in doc:
-#         tables = page.find_tables()
-#         table = tables[0]
-#         page_df = table.to_pandas()
-#         page_df_lst.append(page_df)
-
-#     df = pd.concat(page_df_lst)
-#     df.to_csv(f"csv_data/{filename}.csv",index =False)
-#     return  
-
-# filename = "EB_Purchase_Details"
-# convert(filename)
-# filename = "EB_Redemption_Details"
-# convert(filename)
